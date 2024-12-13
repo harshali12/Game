3e6aac691c0bcdd287e43d138b79fa203cfcd7eb
harshali12 <50790522+harshali12@users.noreply.github.com>
2024-12-13 12:31:16 +0530

    Add files via upload

---
+++ StonePaperScissor/StonePaperScissor/StonePaperScissorApp.swift
@@ -0,0 +1,16 @@
+//
+//  StonePaperScissorApp.swift
+//  StonePaperScissor
+//
+//  Created by Harshali on 13/12/24.
+//
+
+import SwiftUI
+
+struct StonePaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
